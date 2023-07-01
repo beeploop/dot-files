eb1e1255e207aeed842562064d08ac36dd50b371
John Loyd Mulit <jlmulit68@gmail.com>
2023-07-01 17:18:54 +0800

    update remap for formatting code with lsp

--- lua/dui/remap.lua
+++ lua/dui/remap.lua
@@ -44,7 +44,7 @@ vim.keymap.set("i", "jk", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set("n", "ff", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
