e51a246d1dc5df354931910f52c839be0240bdaa
John Loyd Mulit <jlmulit68@gmail.com>
2024-03-26 07:15:27 +0800

    fix: cnext and cprev remap conflict with harpoon

--- lua/dui/remap.lua
+++ lua/dui/remap.lua
@@ -41,8 +41,8 @@ vim.keymap.set("i", "jk", "<Esc>")
 
 vim.keymap.set("n", "ff", vim.lsp.buf.format)
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>]", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<leader>[", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
