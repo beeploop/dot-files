0a79b97fbad53f979e05cd45f0ae93c5fac1969a
John Loyd Mulit <jlmulit68@gmail.com>
2024-09-09 09:05:20 +0800

    chore: cleanup remaps

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -12,25 +12,20 @@ vim.keymap.set({ "n", "v" }, "p", '"+p')
 
 --vim.keymap.set("n", "<leader>ee", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>ee", function()
-    local filetype = vim.api.nvim_buf_get_option(0, "filetype")
-    if filetype == "netrw" then
-        vim.cmd("bprevious")
-    else
-        vim.cmd("Ex")
-    end
+	local filetype = vim.api.nvim_buf_get_option(0, "filetype")
+	if filetype == "netrw" then
+		vim.cmd("bprevious")
+	else
+		vim.cmd("Ex")
+	end
 end)
 
 -- Add a key mapping for inserting the code snippet
-vim.api.nvim_set_keymap('i', '/err', 'if err != nil {\nreturn err\n}<ESC>', { noremap = true })
+vim.api.nvim_set_keymap("i", "/er", "if err != nil {\nreturn err\n}<ESC>", { noremap = true })
 
 --shortcut to focus on the nvim tree
 vim.keymap.set("n", "<leader>ef", function()
-    vim.cmd("NvimTreeFocus")
-end)
-
--- shortcut for opening fugitive Git
-vim.keymap.set("n", "<leader>gs", function()
-    vim.cmd("Git")
+	vim.cmd("NvimTreeFocus")
 end)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -43,7 +38,7 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 -- This is going to get me cancelled
-vim.keymap.set("i", "jk", "<Esc>")
+vim.keymap.set("i", "kj", "<Esc>")
 
 -- vim.keymap.set("n", "ff", vim.lsp.buf.format)
 
@@ -53,24 +48,19 @@ vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 -- rename all instances of word under the cursor
-vim.keymap.set("n", "<leader>ss", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+-- vim.keymap.set("n", "<leader>ss", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- make it rain
-vim.keymap.set("n", "<leader>mir", "<cmd>CellularAutomaton make_it_rain<CR>");
-vim.keymap.set("n", "<leader>gol", "<cmd>CellularAutomaton game_of_life<CR>");
+-- vim.keymap.set("n", "<leader>mir", "<cmd>CellularAutomaton make_it_rain<CR>")
+-- vim.keymap.set("n", "<leader>gol", "<cmd>CellularAutomaton game_of_life<CR>")
 
 -- source the current file
 vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
+	vim.cmd("so")
 end)
 
 --shortcut to view list of error with Telescope
-vim.keymap.set("n", "<leader>dl", "<cmd>Telescope diagnostics<CR>")
-
--- wanna see what I look like in the inside??? You can also do this with Shift+k
-vim.keymap.set("n", "<leader>df", function()
-    vim.cmd("lua vim.lsp.buf.hover()")
-end)
+-- vim.keymap.set("n", "<leader>dl", "<cmd>Telescope diagnostics<CR>")
 
 -- vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end)
 -- vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end)
