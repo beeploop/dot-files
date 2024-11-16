986c64e67f6665ec01d092cbf108279457d0135d
John Loyd Mulit <jlmulit68@gmail.com>
2024-11-16 23:16:11 +0800

    feat: snippets for flutter

---
+++ lua/plugins/luasnip.lua
@@ -0,0 +1,83 @@
+return {
+	"L3MON4D3/LuaSnip",
+	version = "v2.3.0",
+	build = "make install_jsregexp",
+	config = function()
+		local ls = require("luasnip")
+		local s = ls.snippet
+		local t = ls.text_node
+		local i = ls.insert_node
+		local extras = require("luasnip.extras")
+		local rep = extras.rep
+		local fmt = require("luasnip.extras.fmt").fmt
+
+		ls.config.set_config({
+			history = true,
+			updateevents = "TextChanged,TextChangedI",
+		})
+
+		vim.keymap.set({ "i", "s" }, "<C-l>", function()
+			if ls.expand_or_jumpable() then
+				return ls.expand_or_jump()
+			end
+		end, { silent = true })
+
+		vim.keymap.set({ "i", "s" }, "<C-j>", function()
+			if ls.jumpable(-1) then
+				return ls.jump(-1)
+			end
+		end, { silent = true })
+
+		-- Dart snippet
+		ls.add_snippets("dart", {
+			s(
+				"stl",
+				fmt(
+					[[
+            import 'package:flutter/material.dart';
+
+            class {} extends StatelessWidget {{
+                const {}({{super.key}});
+
+                @override
+                Widget build(BuildContext context) {{
+                    throw UnimplementedError();
+                }}
+            }}
+            ]],
+					{ i(1), rep(1) }
+				)
+			),
+			s(
+				"stf",
+				fmt(
+					[[
+                    import 'package:flutter/material.dart';
+
+                    class {} extends StatefulWidget {{
+                        const {}({{super.key}});
+
+                        @override
+                        State<{}> createState() => _{}State();
+                    }}
+
+                    class _{}State extends State<{}> {{
+                        @override
+                        Widget build(BuildContext context) {{
+                            return Scaffold();
+                        }}
+                    }}
+                    ]],
+					{
+						i(1),
+						rep(1),
+						rep(1),
+						rep(1),
+						rep(1),
+						rep(1),
+					}
+				)
+			),
+		})
+	end,
+}
