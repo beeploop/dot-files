040d3e368aa0e97a85c65fa24af57058cbadad2c
John Loyd Mulit <jlmulit68@gmail.com>
2025-03-29 09:11:08 +0800

    fix: undo tree show and toggle

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,7 +1,9 @@
 return {
-    "mbbill/undotree",
-    config = function()
-        vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
-    end
+	"mbbill/undotree",
+	config = function()
+		vim.keymap.set("n", "<leader>uu", function()
+			vim.cmd.UndotreeToggle()
+			vim.cmd.UndotreeFocus()
+		end, { desc = "Undotree: Toggle and focus" })
+	end,
 }
-
--- lua/plugins/zen_mode.lua
+++ lua/plugins/zen_mode.lua
@@ -1,14 +1,24 @@
 return {
 	"folke/zen-mode.nvim",
-	opts = {},
-	config = function()
-		require("zen-mode").setup({
-			plugins = {
-				options = {
-					laststatus = 3,
-				},
-			},
-		})
-		vim.keymap.set("n", "<leader>zz", "<cmd>ZenMode<CR>")
+	dependencies = {
+		"folke/twilight.nvim", -- Optional: for code focusing
+	},
+	opts = {
+		window = {
+			width = 90,
+			options = {},
+		},
+		plugins = {
+			options = {},
+			twilight = { enabled = true },
+			tmux = { enabled = false },
+		},
+	},
+	config = function(_, opts)
+		local zen_mode = require("zen-mode")
+		zen_mode.setup(opts)
+
+		-- Set up the keymap to toggle Zen Mode
+		vim.keymap.set("n", "<leader>zz", zen_mode.toggle, { desc = "Toggle Zen Mode" })
 	end,
 }
